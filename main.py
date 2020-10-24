dd6f18c9b7fabf76aded9a2f8542428eec825cab
anjali <guptaanj7786@gmail.com>
2020-10-24 09:19:59 +0530

    Room registration page added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template,request,redirect,url_for,session
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import os,uuid
+
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -14,78 +17,85 @@ app.config['MYSQL_DB'] = 'propertymanagement'
 
 mysql = MySQL(app)
 
-@app.route("/",methods =['GET', 'POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    msg=""
+    msg = ""
     if request.method == 'POST':
         loc = request.form['location']
         option = request.form['options']
         cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        if loc=="" and option=="appartments":
-            cur.execute('SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail')
+        if loc == "" and option == "appartments":
+            cur.execute(
+                'SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail')
             result = cur.fetchall()
             return render_template('index.html', detail=result, msg="Result for the search")
-        elif loc=="" and option=="rooms":
+        elif loc == "" and option == "rooms":
             cur.execute('SELECT * FROM room')
             result = cur.fetchall()
             return render_template('index.html', detail=result, msg="Result for the search")
-        if option=="appartments":
-            cur.execute('SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail where State = % s', [loc])
+        if option == "appartments":
+            cur.execute(
+                'SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail where State = % s',
+                [loc])
             result = cur.fetchall()
-        elif option=="rooms":
+        elif option == "rooms":
             cur.execute('SELECT * FROM room where STATE = % s', [loc])
             result = cur.fetchall()
         mysql.connection.commit()
         cur.close()
         if result:
-            return render_template('index.html', detail=result,msg="Result for the search")
+            return render_template('index.html', detail=result, msg="Result for the search")
         else:
             return render_template('index.html', detail="No records found")
     else:
         return render_template('index.html')
 
+
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/login/", methods=['GET','POST'])
+
+@app.route("/login/", methods=['GET', 'POST'])
 def login():
     msg = ''
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if len(username)>0 and len(password)>0:
-         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-         cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
-         account = cursor.fetchone()
-         mysql.connection.commit()
-         cursor.close()
-         if account:
-             if username=='admin' and password=='admin':
-              session['loggedin'] = True
-              session['id'] = account['id']
-              session['username'] = account['username']
-              return redirect(url_for('dashboard'))
-             else:
-                 session['loggedin'] = True
-                 session['id'] = account['id']
-                 session['username'] = account['username']
-                 return redirect(url_for('userdashboard'))
-         else:
-            msg = 'Incorrect username/password!'
+        if len(username) > 0 and len(password) > 0:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+            account = cursor.fetchone()
+            mysql.connection.commit()
+            cursor.close()
+            if account:
+                if username == 'admin' and password == 'admin':
+                    session['loggedin'] = True
+                    session['id'] = account['id']
+                    session['username'] = account['username']
+                    return redirect(url_for('dashboard'))
+                else:
+                    session['loggedin'] = True
+                    session['id'] = account['id']
+                    session['username'] = account['username']
+                    return redirect(url_for('userdashboard'))
+            else:
+                msg = 'Incorrect username/password!'
         else:
-          msg = ' Please fill the entries !'
+            msg = ' Please fill the entries !'
     return render_template('login.html', msg=msg)
+
 
 @app.route('/login/logout')
 def logout():
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
-@app.route("/register/", methods=['GET','POST'])
+@app.route("/register/", methods=['GET', 'POST'])
 def register():
     msg = ''
     if request.method == 'POST':
@@ -95,37 +105,39 @@ def register():
         email = request.form['email']
         mobile = request.form['mobile']
         cpassword = request.form['cpassword']
-        if len(username)>0 and len(password)>0 and len(email)>0 and len(mobile)>0 and len(fullname)>0 and len(cpassword)>0:
-         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-         cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
-         account = cursor.fetchone()
-         cursor.execute('SELECT * FROM accounts WHERE email = % s', (email,))
-         account1 = cursor.fetchone()
-         cursor.execute('SELECT * FROM accounts WHERE mobile = % s', (mobile,))
-         account2 = cursor.fetchone()
-         if account:
-            msg = 'Account already exists with this username!'
-         elif account1:
-            msg= 'Account already exists with this email !'
-         elif account2:
-            msg= 'Account already exists with this mobile number !'
-         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-         elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-         elif not username or not password or not email or not mobile or not cpassword or not fullname:
-            msg = 'Please fill out the form !'
-         elif len(mobile) != 10:
-            msg = 'Enter 10 digit number !'
-         elif cpassword != password:
-            msg = 'Confirm password does not match with password !'
-         else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s,% s,% s,% s)', (username, fullname, email, mobile, password, cpassword,))
-            mysql.connection.commit()
-            cursor.close()
-            msg = 'You have successfully registered !'
+        if len(username) > 0 and len(password) > 0 and len(email) > 0 and len(mobile) > 0 and len(fullname) > 0 and len(
+                cpassword) > 0:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
+            account = cursor.fetchone()
+            cursor.execute('SELECT * FROM accounts WHERE email = % s', (email,))
+            account1 = cursor.fetchone()
+            cursor.execute('SELECT * FROM accounts WHERE mobile = % s', (mobile,))
+            account2 = cursor.fetchone()
+            if account:
+                msg = 'Account already exists with this username!'
+            elif account1:
+                msg = 'Account already exists with this email !'
+            elif account2:
+                msg = 'Account already exists with this mobile number !'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address !'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                msg = 'Username must contain only characters and numbers !'
+            elif not username or not password or not email or not mobile or not cpassword or not fullname:
+                msg = 'Please fill out the form !'
+            elif len(mobile) != 10:
+                msg = 'Enter 10 digit number !'
+            elif cpassword != password:
+                msg = 'Confirm password does not match with password !'
+            else:
+                cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s,% s,% s,% s)',
+                               (username, fullname, email, mobile, password, cpassword,))
+                mysql.connection.commit()
+                cursor.close()
+                msg = 'You have successfully registered !'
         else:
-           msg = 'Please fill out the form !'
+            msg = 'Please fill out the form !'
 
     return render_template('register.html', msg=msg)
 
@@ -152,7 +164,7 @@ def registeredusers():
         resultValue = cur.execute("SELECT * FROM accounts")
         if resultValue > 0:
             userDetails = cur.fetchall()
-            return render_template('registeredusers.html', userDetails=userDetails,username=session['username'])
+            return render_template('registeredusers.html', userDetails=userDetails, username=session['username'])
 
 
 @app.route('/userdashboard/apmt_reg/', methods=['GET', 'POST'])
@@ -171,53 +183,98 @@ def apmt_reg():
         pin = details['Pincode']
         state = details['State']
         country = details['Country']
-        # Image = details['Apmt_Img']
-        #if len(mobile) != 10:
-           # msg = 'Enter 10 digit number'
-       # elif len(pin) != 10:
-          #  msg = 'Enter 6 digit number'
-       # else:
+        atype = details['Atype']
+        rs = details['Rent/Sale']
+        availability = details['status']
+        Price = details['Price']
+        facilities = details['Facilities']
+        description = details['Description']
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)
+        f_name = str(uuid.uuid4()) + str(extension)
+        app.config['UPLOAD_FOLDER'] = 'static/Uploads'
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO apartmentdetail VALUES(NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                        (apmtname, email, mobile, plot_no, address, landmark, city, pin, state, country))
+        cur.execute("INSERT INTO apartmentdetail VALUES(NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (apmtname, email, mobile, plot_no, address, landmark, city, pin, state, country, atype, rs, availability,Price,facilities,description,file))
         mysql.connection.commit()
         cur.close()
-            #msg = 'Registration Successful! Thank You !'
-    return render_template('Apmt_reg.html',username=session['username'])
+        # msg = 'Registration Successful! Thank You !'
+    return render_template('Apmt_reg.html', username=session['username'])
 
-@app.route("/home",methods =['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 def home2():
-    msg=""
+    msg = ""
     if request.method == 'POST':
         loc = request.form['location']
         option = request.form['options']
         cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        if loc=="" and option=="appartments":
-            cur.execute('SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail')
+        if loc == "" and option == "appartments":
+            cur.execute(
+                'SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail')
             result = cur.fetchall()
-            return render_template('home.html', detail=result, msg="Result for the search",username=session['username'])
-        elif loc=="" and option=="rooms":
-            cur.execute('SELECT * FROM room')
+            return render_template('home.html', detail=result, msg="Result for the search",
+                                   username=session['username'])
+        elif loc == "" and option == "rooms":
+            cur.execute('SELECT * FROM rooms')
             result = cur.fetchall()
-            return render_template('home.html', detail=result, msg="Result for the search",username=session['username'])
-        if option=="appartments":
-            cur.execute('SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail where State = % s', [loc])
+            return render_template('home.html', detail=result, msg="Result for the search",
+                                   username=session['username'])
+        if option == "appartments":
+            cur.execute(
+                'SELECT Aname,Email,Mobile,Plot_no,Address,Landmark,City,Pincode,State,Country FROM apartmentdetail where State = % s',
+                [loc])
             result = cur.fetchall()
-        elif option=="rooms":
-            cur.execute('SELECT * FROM room where STATE = % s', [loc])
+        elif option == "rooms":
+            cur.execute('SELECT	fname, Email, Mobile, AltMobile, Plot_no, Address, Landmark, City, Pincode, State, Country, AvailRoom, RS, Availability ,Deposit, Facilities, Dscrption where STATE = % s', [loc])
             result = cur.fetchall()
         mysql.connection.commit()
         cur.close()
         if result:
-            return render_template('home.html', detail=result, msg="Result for the search", username=session['username'])
+            return render_template('home.html', detail=result, msg="Result for the search",
+                                   username=session['username'])
         else:
             return render_template('home.html', detail="No records found", username=session['username'])
     else:
         return render_template('home.html', username=session['username'])
 
+
 @app.route("/about2")
 def about2():
     return render_template('about2.html', username=session['username'])
+
+
+@app.route('/userdashboard/room_reg/', methods=['GET', 'POST'])
+def room_reg():
+    msg = ''
+    if request.method == 'POST':
+        # fetch data
+        details = request.form
+        email = details['Email']
+        mobile = details['Mobile']
+        plot_no = details['Plot']
+        address = details['Address']
+        landmark = details['Landmark']
+        city = details['City']
+        pin = details['Pincode']
+        state = details['State']
+        country = details['Country']
+        availability = details['status']
+        rent = details['Rent']
+        facilities = details['Facilities']
+        description = details['Description']
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)
+        f_name = str(uuid.uuid4()) + str(extension)
+        app.config['UPLOAD_FOLDER'] = 'static/Uploads'
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO roomdetail VALUES(NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (email, mobile, plot_no, address, landmark, city, pin, state, country, availability, rent, facilities, description, file))
+        mysql.connection.commit()
+        cur.close()
+        # msg = 'Registration Successful! Thank You !'
+    return render_template('roomreg.html', username=session['username'])
 
 
 app.run(debug=True)
--- propertymanagement.sql
+++ propertymanagement.sql
@@ -1,38 +1,60 @@
 CREATE DATABASE propertymanagement;
 
-USE propertymanagement;
-
+USE propertymanagement; 
 
 CREATE TABLE  accounts (
-	id int(11) NOT NULL AUTO_INCREMENT,
+  	id int(11) NOT NULL AUTO_INCREMENT,
   	username varchar(50) NOT NULL,
-    fullname varchar(50) NOT NULL,
-    email varchar(50) NOT NULL,
-    mobile varchar(10) NOT NULL,
-  	password varchar(255) NOT NULL,
-    cpassword varchar(255) NOT NULL,
-    PRIMARY KEY (id)
-);
+      fullname varchar(50) NOT NULL,
+      email varchar(50) NOT NULL,
+      mobile varchar(10) NOT NULL,
+	  password varchar(255) NOT NULL,
+ 	  cpassword varchar(255) NOT NULL,
+      PRIMARY KEY (id)
+); 
 
 CREATE TABLE apartmentdetail (
-  A_ID int NOT NULL AUTO_INCREMENT,
-  Aname varchar(50) NOT NULL,
-  Email varchar(100) NOT NULL,
-  Mobile decimal(10,0) NOT NULL,
-  Plot_no int NOT NULL,
-  Address varchar(100) NOT NULL,
-  Landmark varchar(100) NOT NULL,
-  City varchar(50) NOT NULL,
-  Pincode decimal(6,0) NOT NULL,
-  State varchar(50) NOT NULL,
-  Country varchar(50) NOT NULL,
-  PRIMARY KEY (A_ID)
+    A_ID int NOT NULL AUTO_INCREMENT,
+    Aname varchar(50) NOT NULL,
+    Email varchar(100) NOT NULL,
+    Mobile decimal(10,0) NOT NULL,
+    Plot_no int NOT NULL,
+    Address varchar(100) NOT NULL,
+    Landmark varchar(100) NOT NULL,
+    City varchar(50) NOT NULL,
+    Pincode decimal(6,0) NOT NULL,
+    State varchar(50) NOT NULL,
+    Country varchar(50) NOT NULL,
+    Atype varchar(50) NOT NULL,
+    RS varchar(4) NOT NULL,
+ 	Availability varchar(20) NOT NULL,
+	Price numeric NOT NULL,
+ 	Facilities varchar(250) NOT NULL,
+ 	Dscrption varchar(500) NOT NULL,
+ 	image blob NOT NULL,
+    PRIMARY KEY (A_ID)
 )
+
+CREATE TABLE roomdetail(
+ 	R_ID int NOT NULL AUTO_INCREMENT,
+	Email varchar(100) NOT NULL,
+ 	Mobile numeric(10) NOT NULL,
+ 	Plot_no int NOT NULL,
+ 	Address varchar(100) NOT NULL,
+ 	Landmark varchar(100) NOT NULL,
+ 	City varchar(50) NOT NULL,
+ 	Pincode decimal(6,0) NOT NULL,
+ 	State varchar(50) NOT NULL,
+ 	Country varchar(50) NOT NULL,
+ 	Availability varchar(20) NOT NULL,
+ 	Rent numeric NOT NULL,
+	Facilities varchar(250) NOT NULL,
+ 	Dscrption varchar(500) NOT NULL,
+    image blob NOT NULL,
+ 	PRIMARY KEY (R_ID)
+ )
 
 
 INSERT INTO accounts (id, username, fullname,email,mobile,password,cpassword) VALUES (1, 'admin', 'admin','admin@gmail.com','1234567890','admin','admin');
 
-select * from accounts;
-  
-  
-
+Select * from roomdetail;
